0746a8d60c341e87f1115811cfd946d0778ae377
YugaRavi <yugapriya03@gmail.com>
2022-10-17 16:13:17 +0530

    evening

---
+++ src/org/company/CompanyInfo.java
@@ -0,0 +1,19 @@
+package org.company;
+
+public class CompanyInfo {
+	private void companyName() {
+		System.out.println("Greens");
+	}
+private void companyId() {
+	System.out.println("123");
+}
+private void companyAddress() {
+	System.out.println("chennai");
+}
+public static void main(String[] args) {
+	CompanyInfo k =new CompanyInfo();
+	k.companyName();
+	k.companyId();
+	k.companyAddress();
+}
+}
